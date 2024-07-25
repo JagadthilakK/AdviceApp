f5ddd909da56392f6b5df12518423bff7d7895df
Jagadthilak <jagadthilak00@gmail.com>
2024-07-25 19:17:53 +0530

    advice app full project

---
+++ src/Component/Advise.jsx
@@ -0,0 +1,36 @@
+import "../StyleSheet/Advice.css"
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
+// Created this for basic api integration
+export const Advice= ()=>{
+
+    const [advice,setAdvice] = useState("Click get advise");
+    
+    // const getAdvice= async ()=>{
+    //     const response = await fetch("https://api.adviceslip.com/advice");
+    //     const data = await response.json();
+    //     setAdvice(data.slip.advice);
+    // }
+
+    const getAdvice = async ()=>{
+        const response = await axios.get("https://api.adviceslip.com/advice")
+        setAdvice(response.data.slip.advice);
+    }
+
+    useEffect(()=>{
+        getAdvice();
+    },[])
+
+    return(
+    <>  
+        <div className="mainContainer">
+            <div className="header">Get Advise</div>
+            <div className="bodyContainer">
+                <h1 className="adviceText">{advice}</h1>
+                <button className="getButton" onClick={getAdvice}>Get advise</button>
+            </div> 
+        </div> 
+    </>
+)}
